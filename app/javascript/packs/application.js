b9b8c01d1e64d0ca3f903e957131175533f3119c
zeronosu77108 <taiga77108@gmail.com>
2019-10-26 16:38:13 +0900

    WIP calendar rendering test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,14 +7,16 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
 
-//= require moment 
-//= require fullcalendar
-//= require fullcalendar/locale-all
+require("jquery")
 
-$('#calendar').fullCalendar({});
+require("moment")
+require("fullcalendar")
 
+$(document).ready(function(){
+  $('#calendar').fullCalendar({
+  });
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
