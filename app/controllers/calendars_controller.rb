116868d633154d7cea7f851aed9891d0909e86aa
Kohei Kina <e165747@ie.u-ryukyu.ac.jp>
2019-10-27 11:17:06 +0900

    change routes

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,2 +1,5 @@
 class CalendarsController < ApplicationController
+  def index
+    redirect_to '/home/index' if not user_signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: 'calendars#index'
   get 'home/index'
-  
   devise_for :users, :controllers => {
     :omniauth_callbacks =>  "users/omniauth_callbacks"
   }
