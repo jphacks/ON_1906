ef61b56d930e94f2478f1ccf32b666db827afeff
Kohei Kina <e165747@ie.u-ryukyu.ac.jp>
2019-10-27 12:41:40 +0900

    add function logout and create logout button

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :omniauth_callbacks =>  "users/omniauth_callbacks"
   }
+
+  devise_scope :user do
+    get '/users/log_out' => 'devise/sessions#destroy'
+  end
 end
